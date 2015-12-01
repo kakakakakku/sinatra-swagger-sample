fe8f540d3cf40723362b9582a65d984acf3c635d
kakakakakku <y.yoshida22@gmail.com>
2015-12-01 16:05:28 +0900

    Added users/:id endpoint

--- app.rb
+++ app.rb
@@ -6,15 +6,22 @@ configure do
   enable :cross_origin
 end
 
+users = [
+  { id: 1, name: 'User1' },
+  { id: 2, name: 'User2' },
+  { id: 3, name: 'User3' },
+  { id: 4, name: 'User4' },
+  { id: 5, name: 'User5' }
+]
+
 get '/users' do
   content_type :json
-  [
-    { id: 1, name: 'User1' },
-    { id: 2, name: 'User2' },
-    { id: 3, name: 'User3' },
-    { id: 4, name: 'User4' },
-    { id: 5, name: 'User5' }
-  ].to_json
+  users.to_json
+end
+
+get '/users/:id' do
+  content_type :json
+  users.select { |user| user[:id] == params[:id].to_i }.to_json
 end
 
 get '/resources' do
